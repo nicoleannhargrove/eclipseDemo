1f0ffa4c31c158a6629179420a3321b627aa9325
nicolehargrove <nicolehargrove@colemacbp.attlocal.net>
2022-04-14 00:04:41 -0400

    Second time around

--- tech.strategio.HelloWorldGitHub/src/tech/strategio/HelloWorldGitHub/HelloWorldGitHubTest.java
+++ tech.strategio.HelloWorldGitHub/src/tech/strategio/HelloWorldGitHub/HelloWorldGitHubTest.java
@@ -4,7 +4,7 @@ public class HelloWorldGitHubTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World to Everyone GitHub is Integrated");
+		System.out.println("Second Time around Hello World to Everyone GitHub is Integrated");
 	}
 
 }
