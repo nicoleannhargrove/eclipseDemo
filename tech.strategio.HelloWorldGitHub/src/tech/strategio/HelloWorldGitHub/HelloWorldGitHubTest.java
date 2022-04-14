a38f8750f47720fc99a05beefb8c98692ebea26b
nicolehargrove <nicolehargrove@colemacbp.attlocal.net>
2022-04-14 00:01:48 -0400

    First Commit from Eclipse

---
+++ tech.strategio.HelloWorldGitHub/src/tech/strategio/HelloWorldGitHub/HelloWorldGitHubTest.java
@@ -0,0 +1,10 @@
+package tech.strategio.HelloWorldGitHub;
+
+public class HelloWorldGitHubTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World to Everyone GitHub is Integrated");
+	}
+
+}
